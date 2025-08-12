786fac41804df20fae7454080d1323be9f7b1805
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2025-08-12 03:43:50 +0000

    - C# project setup

---
+++ c-sharp-kata/Program.cs
@@ -0,0 +1,12 @@
+﻿using c_sharp_kata.Solutions;
+
+namespace c_sharp_kata;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Test.HelloWorld();
+        
+    }
+}
---
+++ c-sharp-kata/Solutions/Test.cs
@@ -0,0 +1,7 @@
+namespace c_sharp_kata.Solutions;
+public class Test{
+    public static void HelloWorld()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
