49a614ebf9791e9d084f0385c6154334ff471ae1
ジョナサン ラモン <@DESKTOP-GGKQM2J.>
2025-08-21 22:21:02 -0400

    - Started work on KataUser

--- c-sharp-kata/Solutions/CodewarsRank.cs
+++ c-sharp-kata/Solutions/CodewarsRank.cs
@@ -6,11 +6,16 @@ namespace c_sharp_kata.Solutions;
  Write a class called User that is used to calculate the amount that a user will progress through a ranking system similar to the one Codewars uses. 
 */
 
+// REQUIREMENTS
+// when rank == -1 then rank += 2  (skip zero)
+
 public class KataUser
 {
-    public int rank;
-    public int progress;
+    public int rank { get; private set; }
+    public int progress { get; private set; }
 
+     static readonly int maxRank = 8;
+     static readonly int minRank = -8;
     public KataUser()
     {
         rank = -8; // User starts at rank -8
@@ -18,18 +23,48 @@ public class KataUser
 
     }
 
+    public void RankChecker() {
+        // if rank < -8 or rank > 8 then ERROR
+        if (rank < minRank || rank > maxRank)
+        {
+            throw new Exception("ERROR: Rank value out of bounds.");
+        } 
+    }
+
     public void IncProgress(int actRank)
     {
-        // REQUIREMENTS
-            // when progress == 100 then rank++
-            // if progress > 100 then progress =  progress - 100
-            // when rank == -1 then rank += 2  (skip zero)
-            // maxRank = 8
-            // if rank < -8 or rank > 8 then ERROR
-            // when actRank == rank then progress += 3
-            // when actRank == rank-- then progress++
-            // when actRank < rank-- then continue
-            // when actRank > rank then progress += 10 * (actRank - rank) * (actRank - rank)
+
+        // when progress == 100 then rank++
+        // if progress > 100 then progress =  progress - 100
+        if (progress >= 100)
+        {
+            progress += progress - 100;
+            rank++;
+        }
+
+        // when actRank == rank then progress += 3
+        if (actRank == rank)
+        {
+            progress += 3;
+        }
+        // when actRank == rank-- then progress++
+        else if (actRank == rank--)
+        {
+            progress++;
+        }
+        // when actRank < rank-- then continue
+        else if (actRank < rank--)
+        {
+            return;
+        }
+        // when actRank > rank then progress += 10 * (actRank - rank) * (actRank - rank)
+        else if (actRank > rank)
+        {
+            progress += 10 * (actRank - rank) * (actRank - rank);
+        }
+        
+        // TODO: Explore modularizing further for readablility and practice
+
 
     }
 }
