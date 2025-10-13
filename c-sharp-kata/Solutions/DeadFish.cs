a6b7564814bb8de5fd79de5ffc61faf890ecf57b
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2025-10-13 03:17:40 +0000

    Began implementation of Solution

---
+++ c-sharp-kata/Solutions/DeadFish.cs
@@ -0,0 +1,20 @@
+namespace c_sharp_kata.Solutions;
+
+public class DeadFish 
+{
+  public static int[] Parse(string data)
+  {
+    Char[] letters = data.ToCharArray();
+    int[] res = [];
+    // iterate through each letter
+    // with switch case
+    /*
+    i: Increment the value
+    d: Decrement the value
+    s: Square the value
+    o: Output(Add) the value to a result array 
+    */
+
+    return []; // placeholder return 
+  }
+}
