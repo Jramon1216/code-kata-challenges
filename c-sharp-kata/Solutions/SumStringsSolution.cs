fed5595ff75b77752b5c3ad2be00abe1193c0df0
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2026-03-13 12:11:24 +0000

    - Initial set up of Sum Strings as numbers kata solution

---
+++ c-sharp-kata/Solutions/SumStringsSolution.cs
@@ -0,0 +1,20 @@
+namespace c_sharp_kata.Solutions;
+
+/*
+    Sum Strings as Numbers - https://www.codewars.com/kata/5324945e2ece5e1f32000370
+    Given the string representations of two integers, return the string representation of the sum of those integers.
+
+    For example:
+
+    sumStrings('1','2') // => '3'
+
+    A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+*/
+
+public class SumStringsSolution
+{
+    public static string sumStrings(string a, string b)
+    {
+      return "";
+    }
+} 
