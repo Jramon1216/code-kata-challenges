11d9623c162c6d9adbd23f9e725a6e0c9abde35a
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2026-03-17 02:05:35 +0000

    - updated Getting to know LINQ w/ problem description

--- c-sharp-kata/Solutions/GettingToKnowLINQSolution.cs
+++ c-sharp-kata/Solutions/GettingToKnowLINQSolution.cs
@@ -1,8 +1,17 @@
 namespace c_sharp_kata.Solutions;
-
-using System.ComponentModel.Design;
 using System.Linq;
 
+/*
+Getting to know LINQ - https://www.codewars.com/kata/581d2647b2d02e33be000094/
+
+
+In .NET the Language Integrated Query (LINQ) component adds several helpful methods that can be used with enumerables.
+
+The goal of this Kata is to practice some scenarios where LINQ can be used to replace loops.
+
+Replace each loop in the code with a LINQ expression. The validation will fail if the words 'for' or 'while' occurs anywhere in the code (even in comments).
+
+*/
 public class GettingToKnowLINQSolution
 {
     public static int Sum(int[] integers)
