2fe904a555978d0d49ddcfc6c31153ee02f8810d
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2026-03-29 20:29:45 +0000

    - Implemented  Sum Strings as Numbers solution

--- c-sharp-kata/Solutions/SumStringsSolution.cs
+++ c-sharp-kata/Solutions/SumStringsSolution.cs
@@ -1,5 +1,6 @@
-namespace c_sharp_kata.Solutions;
+using System.Numerics;
 
+namespace c_sharp_kata.Solutions;
 /*
     Sum Strings as Numbers - https://www.codewars.com/kata/5324945e2ece5e1f32000370
     Given the string representations of two integers, return the string representation of the sum of those integers.
@@ -13,8 +14,35 @@ namespace c_sharp_kata.Solutions;
 
 public class SumStringsSolution
 {
-    public static string sumStrings(string a, string b)
+    public static string SumStrings(string a, string b) 
+    // long soultion without using TryParse
     {
-      return "";
+
+        bool aIsNeg = a.Contains('-'),
+             bIsNeg = b.Contains('-');
+
+        BigInteger numA = 0;
+        BigInteger numB = 0;
+
+        foreach (var character in a) // convert by character
+        {
+            if (character == '-') continue;
+
+            numA = numA * 10 + Int32.Parse(character.ToString());
+        }
+
+        foreach (var character in b)
+        {
+            if (character == '-') continue;
+
+            numB = numB * 10 + Int32.Parse(character.ToString());
+        }
+
+        numA = aIsNeg ? -numA : numA;
+        numB = bIsNeg ? -numB : numB;
+
+        string res = $"{numA + numB}";
+
+        return res;
     }
 } 
