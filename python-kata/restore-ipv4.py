caefede73a13e9fce1be69d303c7df170e8c77b3
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2026-07-22 16:00:55 +0000

    - Added rich comparison methods - Added additional comments

--- python-kata/restore-ipv4.py
+++ python-kata/restore-ipv4.py
@@ -1,4 +1,5 @@
 import re 
+from functools import total_ordering
 
 """
 Restore IPv4Address class - https://www.codewars.com/kata/63a0f3f49547e9001e7ad1ca
@@ -6,7 +7,9 @@ Restore IPv4Address class - https://www.codewars.com/kata/63a0f3f49547e9001e7ad1
 IPv4Address class has been corrupted by a malevolent team lead. As a most experienced person in the team, you're tasked to restore the missing code. On the bright side, all the unit tests have been left intact. However, you have a gut feeling that you'll need to do something extraordinary…
 """
 
+# TODO: Figure out how to make class not comparable to dynamically created class
 
+@total_ordering
 class IPv4Address: 
     def __init__(self, parts: list[int] | str):  # Type hint to ensure only specified types can be passed in
         """
@@ -14,40 +17,44 @@ class IPv4Address:
 
         :param parts: a list of 4 integers
         """
+        # Validate object using helper function
+        # then return value to make it immutable
         self._parts = self._parts_validator(parts)
     
+    # String method
     def __str__(self) -> str:
         return self._parts
 
-
+    # Rich comparison methods
     def __eq__(self, other): 
         if not isinstance(other, IPv4Address):
             return False
         return self._parts == other._parts
     
-    # TODO: Implement class methods for operators
-    # * They need to return a boolean
-    # * They need to raise a Type error if 'other' is not the correct type
-    # * Look into @total_ordering class decorator
-    # * Compare octet to octet depending on the comparison method
+    def __ne__(self, other) -> bool | object: # Not Equals
+        pointer = 0
 
-    def __ne__(self, other: object) -> bool: # Not Equals
-        pass
+        if not isinstance(other, IPv4Address):
+           return ValueError("Not an instance of an IPv4Address object")
         
-    def __le__(self, other: object) -> bool: # Less than or equal to
-        pass
+        while pointer != len(self._parts):
+            if self._parts[pointer] != other._parts[pointer]:
+                return True
+            else: 
+                pointer += 1
 
-    def __lt__(self, other: object) -> bool: # less than
-        pass
-    
-    def __ge__(self, other: object) -> bool: # Greater than or equal to
-        pass
-
-    def __gt__(self, other: object) -> bool: # Greater than
-        pass
-
-
-
+        return False
+              
+    def __lt__(self, other) -> bool: # less than
+        pointer = 0
+        while pointer != len(self._parts):
+            if self._parts < other._parts:
+                pointer += 1
+            else:
+                return False
+            
+        return True
+                
     def __hash__(self) -> int:
         return hash((self._parts,))
 
@@ -56,7 +63,7 @@ class IPv4Address:
     def parts(self):
         return self._parts
     
-    # Rest of input validation requirements
+    # Helper function for input validation 
     def _parts_validator(self, ipv4) -> str:
         if  ipv4[0] == 0: # First part of address can't be 0
             raise ValueError('Should not create IPv4Address where first part is 0')
@@ -73,7 +80,6 @@ class IPv4Address:
             
             return  ".".join(list(map(str, ipv4)))
 
-
         elif type(ipv4) == str: # if a string is passed in, ensure it's a proper ipv4 address
             # TODO: Change regex to ensure that the first octet cannot be zero
             match = re.fullmatch("^(?:(?:25[0-5]|2[0-4]\\d|1?\\d{1,2})(?:\\.(?!$)|$)){4}$", ipv4) is not None
@@ -84,14 +90,9 @@ class IPv4Address:
     
         raise TypeError("Parts must be list or string")
 
-
     @classmethod
     def from_string(cls, string):
          return cls(string)
 
 
-
-print(str(IPv4Address([1, 2, 3, 4])))
-# print(IPv4Address.from_string('1.2.3.4'))
-# print(f"Hashes are equal? " , hash(IPv4Address([1, 2, 3, 4])) == hash(IPv4Address([4, 2, 3, 2])))
-# print(f"Addresses are equal? ", id(IPv4Address([1, 2, 3, 4])) == id(IPv4Address([4, 2, 3, 2])))
+print(IPv4Address([4,2,3,4]) != IPv4Address([2,2,3,4]))
